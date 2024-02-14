445205c2f3a41223db7f070d7609e8f748f24c9f
SNIAPA <wasilewskihuberts@gmail.com>
2024-02-14 21:11:58 +0100

    logging and unloading

---
+++ inject.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ ! $(id -u) -eq 0 ]]; then
+    echo "This script must not be ran as root!"
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+
+lib=$(realpath 'target/i686-unknown-linux-gnu/debug/liboxide.so')
+pid=$(pidof -s "hl2_linux")
+
+if [[ ! -f $lib ]]; then
+    echo "$lib not found"
+    exit 1
+fi
+
+if [ -z "$pid" ]; then
+   echo "tf2 running"
+   exit 1
+fi
+
+gdb -n -q -batch                                        \
+     -ex "attach $pid"                                  \
+     -ex "set \$dlopen = (void* (*)(char*, int))dlopen" \
+     -ex "set \$dlclose = (int (*)(void*))dlclose"      \
+     -ex "set \$dlerror =  (char* (*)(void))dlerror"    \
+                                                        \
+     -ex "set \$self = \$dlopen(\"$lib\", 6)"   \
+     -ex "call \$dlclose(\$self)"                       \
+     -ex "call \$dlclose(\$self)"                       \
+                                                        \
+     -ex "call \$dlerror()"                             \
+     -ex "detach"                                       \
+     -ex "quit"
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,42 @@
-use std::{thread::{self, sleep}, time::Duration};
 use ctor::{ctor, dtor};
+use libc::{dladdr, dlclose, dlerror, dlopen, Dl_info, RTLD_LAZY, RTLD_NOLOAD};
+use log::{debug, error, info};
 use std::ffi::*;
+use std::{
+    mem::MaybeUninit,
+    thread::{self, sleep},
+    time::Duration,
+};
 
-fn cs(str: &str) -> CString {
-    CString::new(str).unwrap()
+fn src(str: &str) -> *const i8 {
+    CString::new(str).unwrap().as_ptr()
+}
+fn scr(str: *const i8) -> String {
+    unsafe { CStr::from_ptr(str).to_str().unwrap().to_owned() }
 }
 
 #[ctor]
 fn load() {
-    thread::spawn(|| {
-        sleep(Duration::from_secs(5));
-        print!("test");
-        eprint!("teste");
-        unsafe{
-            let handle = libc::dlopen(cs("target/").as_ptr(), libc::RTLD_NOLOAD | libc::RTLD_LAZY);
-            dbg!(handle);
-        }
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
+    thread::spawn(|| unsafe {
+        info!("loading");
+        self_unload();
     });
-
 }
 
-fn self_unload() {
+unsafe fn self_unload() {
+    info!("self unloading");
+    #[allow(invalid_value)]
+    let mut info: Dl_info = MaybeUninit::<Dl_info>::uninit().assume_init();
+    dladdr(load as *const _, &mut info as *mut _);
+    let handle = dlopen(info.dli_fname, RTLD_LAZY | RTLD_NOLOAD);
+    dlclose(handle);
+    dlclose(handle);
 }
 
 #[dtor]
 fn unload() {
-
+    info!("unloaded")
 }
