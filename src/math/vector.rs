2046efea390c385d83cdeb46810edfb5690ae893
SNIAPA <wasilewskihubers@gmail.com>
2024-03-07 17:53:00 +0100

    multipoint basics

--- src/math/vector.rs
+++ src/math/vector.rs
@@ -20,7 +20,7 @@ impl Vector3 {
     pub fn len2d(&self) -> f32 {
         (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
-    pub fn len3d(&self) -> f32 {
+    pub fn len(&self) -> f32 {
         (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
     }
     pub fn angle(&self) -> Angles {
